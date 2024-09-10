5f916557ad610bcccdcaaa9520782212e5a016d8
mysatyam191 <121924000+mysatyam191@users.noreply.github.com>
2024-09-10 11:42:09 +0530

    Delete src directory

--- src/main/java/com/example/demo/features/from/FormRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.features.from;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FormRepository extends JpaRepository<Form, Long> {
-   
-}
--- src/main/java/com/example/demo/features/home/HomeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.features.home;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HomeRepository extends JpaRepository<Home, String>  {
-
-}
